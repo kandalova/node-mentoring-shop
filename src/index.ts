d77431bda4d9c00e8865d4e259712d6fb945ffb9
Leyla Kandalova <leyla_kandalova@epam.com>
2023-11-01 21:20:39 +0600

    Move user db to model

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
-import { findUser } from "./repository/user.repository.ts";
 import cartRouter from "./controller/cart.controller.ts";
 import productRouter from "./controller/product.controller.ts";
 import { ResponseError, getForbidenError, getUnauthorizedError } from "./utils/errors.ts";
 import { getResponseError } from "./utils/utils.ts";
 import { connect, disconnect } from "mongoose";
+import { UserModel } from "./scheme/UserScheme.ts";
 // import { UserModel } from "./scheme/UserScheme.ts";
 // import { runUserSeeder } from "./seeder/userSeeder.ts";
 // import { ProductModel } from "./scheme/ProductScheme.ts";
@@ -14,15 +14,20 @@ export const app = express();
 const PORT = 3000;
 
 const headerHandler = async (req: Request, _: Response, next: NextFunction) => {
-	const userId = req.header('x-user-id') ?? '';
-	if (!userId) {
-		next(getForbidenError());
+	try {
+		const userId = req.header('x-user-id') ?? '';
+		if (!userId) {
+			next(getForbidenError());
+		}
+		const user = await UserModel.findById(userId);
+		if (!user) {
+			next(getUnauthorizedError());
+		}
+		next();
 	}
-	const user = await findUser(userId);
-	if (!user) {
-		next(getUnauthorizedError());
+	catch (err) {
+		next(err);
 	}
-	next();
 }
 
 const errorHandler = async (err: Error | ResponseError, _req: Request, res: Response, _next: NextFunction) => {
@@ -63,5 +68,5 @@ export const init = (async () => {
 process.on("SIGINT", async () => {
 	await disconnect();
 	console.log("Server is disconnected");
-	process.exit(); 
+	process.exit();
 });
